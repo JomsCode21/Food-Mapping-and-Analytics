d337f678479b0e798948fcc846dc354a3340e642
JomsCode21 <iramuhjsolag@gmail.com>
2025-12-17 10:31:38 +0800

    Update gitignore: hide db credentials and user uploads

--- .history/password_hasher_20250810102115.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Replace '1234' with the actual password if different
-$plainPassword = 'bploadmin2025';
-$hashedPassword = password_hash($plainPassword, PASSWORD_DEFAULT);
-echo $hashedPassword;
-?>
